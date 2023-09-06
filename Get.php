517de56d6c44f07597ea391e2b6612ba7a573b8b
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-09-06 14:08:22 -0500

    Create Get.php

---
+++ Get.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = "get";
+include "view-header.php";
+?>
+    <h1>get!</h1>
+<form method = "get" action = "get-result.php">
+  <input type = "text" name = "my-name">
+  <input type = "submit" value = "get data">
+  </form>
+  <?php
+include "view-footer.php";
+?>
