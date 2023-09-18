4f28e7f3cf5e648b2dfc38e854a90e1b56b55b9b
Abigailpidgeon <122913275+Abigailpidgeon@users.noreply.github.com>
2023-09-18 09:45:37 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,22 +4,10 @@ include "view-header.php";
 ?>
     <h1>Post Result</h1>
   <?php
-if (isset($_POST["my-name"])) {
-  ?>
-  <p>The value sent it:</p>
-  <?php
-  echo $_POST["my-name"];
-  } else {
-  ?>
-  <p>Nothing posted to the page<p>
-   <?php
-}
 echo getDisplay()
-}
 include "view-footer.php";
 
 function getDisplay() {
-  <?php
 if (isset($_POST["my-name"])) {
   return "<p>The value sent it:</p>" .  $_POST["my-name"];
   } else { 
